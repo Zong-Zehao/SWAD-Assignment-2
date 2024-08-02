787d5768fde6169ed08449b8174f9bf57f7c65da
izwnsyhrizt <izwnsyhrizt@gmail.com>
2024-08-02 19:11:42 +0800

    Made changes to renter & user

--- Renter.cs
+++ Renter.cs
@@ -1,7 +1,37 @@
 using System;
 
 public class Renter : User
-{
-    private string driversLicenseNo
-    private string renterType
-}
+    {
+        private string driversLicenseNo
+        private string renterType
+    }
+
+    public Renter(string driversLicenseNo, RenterType renterType)
+    {
+        this.driversLicenseNo = driversLicenseNo;
+        this.renterType = renterType;
+    }
+
+    public string DriversLicenseNo
+    {
+        get { return driversLicenseNo; }
+        set { driversLicenseNo = value; }
+    }
+
+    public RenterType RenterType
+    {
+        get { return renterType; }
+        set { renterType = value; }
+    }
+
+    public enum RenterType
+    {
+        Prime,
+        Regular
+    }
+
+public override string ToString()
+    {
+        return $"Driver's License No: {driversLicenseNo}, Renter Type: {renterType}";
+    }
+    }
--- User.cs
+++ User.cs
@@ -1,12 +1,49 @@
 using System;
 
-public class user
+public class User
 {
     private int id;
     private string name;
     private int contact;
-    private datetime dob;
+    private DateTime dob;
+
+    public User(int id, string name, int contact, DateTime dob)
+    {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.dob = dob;
+    }
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Contact
+    {
+        get { return contact; }
+        set { contact = value; }
+    }
+
+    public DateTime Dob
+    {
+        get { return dob; }
+        set { dob = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {id}, Name: {name}, Contact: {contact}, Date of Birth: {dob.ToShortDateString()}";
+    }
 }
 
 
-    
+
