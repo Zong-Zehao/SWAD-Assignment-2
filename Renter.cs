821355506b5f68ffc91daf8a90de2900f3981921
izwnsyhrizt <izwnsyhrizt@gmail.com>
2024-08-02 19:00:06 +0800

    Implemented user and renter

---
+++ Renter.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Renter : User
+{
+    private string driversLicenseNo
+    private string renterType
+}
---
+++ User.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class user
+{
+    private int id;
+    private string name;
+    private int contact;
+    private datetime dob;
+}
+
+
+    
